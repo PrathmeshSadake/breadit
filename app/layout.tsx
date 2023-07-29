af505fbe0810e133ca95b61a8ac68cd3954fa8ca
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-07-29 14:17:40 +0530

    intercepting routes for perfecting the authentication flow

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,8 +16,10 @@ export const metadata = {
 
 export default function RootLayout({
   children,
+  authModal,
 }: {
   children: React.ReactNode;
+  authModal: React.ReactNode;
 }) {
   return (
     <html
@@ -30,6 +32,7 @@ export default function RootLayout({
       <body className='min-h-screen pt-12 bg-slate-50 antialiased'>
         {/* @ts-expect-error Server Component */}
         <Navbar />
+        {authModal}
         <div className='container max-w-7xl mx-auto h-full pt-12'>
           {children}
         </div>
